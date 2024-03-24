c80e09aa1d0e61baf39d2ec2931a0d3add264682
J0nteR <jonathan.ronnas@gmail.com>
2024-03-24 23:37:51 +0100

    Final

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ document.querySelector(".menu-button").addEventListener("click", showMenu)
 
 document.querySelector(".close-button").addEventListener("click", showMenu)
 
-
 function showMenu(){
     let menu = document.querySelector("nav.jSNav")
     menu.classList.toggle("show")  
